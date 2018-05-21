bb6298994ed3b808b1e5bbb838d21f748832a1ec
Ahmed Soliman <ahmed.soliman@programmer.net>
2018-05-21 08:40:25 -0700

    retreving and updating data from db

--- python/CRUD/code/test_db.py
+++ python/CRUD/code/test_db.py
@@ -7,13 +7,32 @@ Base.metadata.bind = engine
 DBSesstion = sessionmaker(bind = engine)
 session = DBSesstion()
 
-myFirstRestaurant = Restaurant(name = "Pizza Palace")
-session.add(myFirstRestaurant)
-session.commit()
+# myFirstRestaurant = Restaurant(name = "Pizza Palace")
+# session.add(myFirstRestaurant)
+# session.commit()
 
-#print(session.query(Restaurant).all())
+# #print(session.query(Restaurant).all())
 
-cheesepizza = MenuItem(name = "Cheese Pizze", course = "some course", price = "price", description = "sone discriptions", restaurant= Restaurant(name = "myFirstRestaurant"))
-session.add(cheesepizza)
-session.commit()
-print(session.query(MenuItem).all())
+# cheesepizza = MenuItem(name = "Cheese Pizze", course = "Pizza", price = "$6.99", description = "sone discriptions", restaurant= Restaurant(name = "myFirstRestaurant"))
+# session.add(cheesepizza)
+# session.commit()
+# print(session.query(MenuItem).all())
+
+print('Start --- ...')
+cheese_pizza = session.query(MenuItem).filter_by(name = 'Cheese Pizze').one()
+
+
+print(cheese_pizza.name)
+print(cheese_pizza.id)
+print(cheese_pizza.price)
+print(cheese_pizza.restaurant.name)
+print("\n")
+# cheese_pizza.price = "$2.99"
+# session.add(cheese_pizza)
+# session.commit()
+
+print(cheese_pizza.name)
+print(cheese_pizza.id)
+print(cheese_pizza.price)
+print(cheese_pizza.restaurant.name)
+print("\n")
